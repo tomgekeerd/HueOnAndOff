92b3c3c57b66dba437ea48e492c6403edb2a60a2
Tom de ruiter <tom@rydee.nl>
2016-02-27 20:05:17 +0100

    Initial Commit
    Initial commit

---
+++ QuickStartApp_OSX/HueQuickStartApp-OSX/PHControlLightsViewController.m
@@ -0,0 +1,203 @@
+/*******************************************************************************
+ Copyright (c) 2013 Koninklijke Philips N.V.
+ All Rights Reserved.
+ ********************************************************************************/
+
+#import "PHControlLightsViewController.h"
+#import <HueSDK_OSX/HueSDK.h>
+#import "AppDelegate.h"
+
+#define MAX_HUE 65535
+
+@interface PHControlLightsViewController ()
+    @property (strong) IBOutlet NSMenu *statusMenu;
+    @property (nonatomic,weak) IBOutlet NSTextField *bridgeIdLabel;
+    @property (nonatomic,weak) IBOutlet NSMenuItem *bridgeIpLabel;
+    @property (nonatomic,weak) IBOutlet NSMenuItem *bridgeLastHeartbeatLabel;
+    @property (nonatomic,weak) IBOutlet NSMenuItem *randomLightsButton;
+
+@end
+
+@implementation PHControlLightsViewController
+@synthesize statusBar = _statusBar;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void)loadView{
+    [super loadView];
+    
+    PHNotificationManager *notificationManager = [PHNotificationManager defaultManager];
+    // Register for the local heartbeat notifications
+    [notificationManager registerObject:self withSelector:@selector(localConnection) forNotification:LOCAL_CONNECTION_NOTIFICATION];
+    [notificationManager registerObject:self withSelector:@selector(noLocalConnection) forNotification:NO_LOCAL_CONNECTION_NOTIFICATION];
+    
+    [self noLocalConnection];
+}
+
+- (void)localConnection{
+    [self loadConnectedBridgeValues];
+}
+
+- (void)noLocalConnection{
+    self.bridgeLastHeartbeatLabel.title = NSLocalizedString(@"Not connected", @"");
+    [self.bridgeLastHeartbeatLabel setEnabled:NO];
+    self.bridgeIpLabel.title = NSLocalizedString(@"Not connected", @"");
+    [self.bridgeIpLabel setEnabled:NO];
+    self.bridgeIdLabel.stringValue = NSLocalizedString(@"Not connected", @"");
+    [self.bridgeIdLabel setEnabled:NO];
+    
+    [self.randomLightsButton setEnabled:NO];
+}
+
+- (void)loadConnectedBridgeValues{
+    PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
+    
+    // Check if we have connected to a bridge before
+    if (cache != nil && cache.bridgeConfiguration != nil && cache.bridgeConfiguration.ipaddress != nil){
+        
+        // Set the ip address of the bridge
+        self.bridgeIpLabel.title = cache.bridgeConfiguration.ipaddress;
+        
+        // Set the identifier of the bridge
+        self.bridgeIdLabel.stringValue = cache.bridgeConfiguration.bridgeId;
+        
+        // Check if we are connected to the bridge right now
+        if (NSAppDelegate.phHueSDK.localConnected) {
+            
+            // Show current time as last successful heartbeat time when we are connected to a bridge
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            [dateFormatter setDateStyle:NSDateFormatterNoStyle];
+            [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
+            
+            self.bridgeLastHeartbeatLabel.title = [NSString stringWithFormat:@"%@",[dateFormatter stringFromDate:[NSDate date]]];
+            
+            [self.randomLightsButton setEnabled:YES];
+        } else {
+            self.bridgeLastHeartbeatLabel.title = NSLocalizedString(@"Waiting...", @"");
+            [self.randomLightsButton setEnabled:NO];
+        }
+    }
+}
+
+- (void) awakeFromNib {
+    
+    [self fileNotifications];
+    
+    self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    self.statusBar.title = @"Hue";
+    
+    // you can also set an image
+    //self.statusBar.image =
+    
+    self.statusBar.menu = self.statusMenu;
+    self.statusBar.highlightMode = YES;
+}
+
+- (IBAction)selectOtherBridge:(id)sender{
+    [NSAppDelegate searchForBridgeLocal];
+}
+
+- (IBAction)randomizeColoursOfConnectLights:(id)sender{
+    [self.randomLightsButton setEnabled:NO];
+    
+    PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
+    PHBridgeSendAPI *bridgeSendAPI = [[PHBridgeSendAPI alloc] init];
+    
+    for (PHLight *light in cache.lights.allValues) {
+        
+        PHLightState *lightState = [[PHLightState alloc] init];
+        
+        [lightState setHue:[NSNumber numberWithInt:arc4random() % MAX_HUE]];
+        [lightState setBrightness:[NSNumber numberWithInt:254]];
+        [lightState setSaturation:[NSNumber numberWithInt:254]];
+
+        // Send lightstate to light
+        [bridgeSendAPI updateLightStateForId:light.identifier withLightState:lightState completionHandler:^(NSArray *errors) {
+            if (errors != nil) {
+                NSString *message = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Errors", @""), errors != nil ? errors : NSLocalizedString(@"none", @"")];
+                
+                NSLog(@"Response: %@",message);
+            }
+            
+            [self.randomLightsButton setEnabled:YES];
+        }];
+    }
+}
+
+- (void) receiveWakeNote: (NSNotification*) note
+{
+    PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
+    PHBridgeSendAPI *bridgeSendAPI = [[PHBridgeSendAPI alloc] init];
+    
+    for (PHLight *light in cache.lights.allValues) {
+        
+        PHLightState *lightState = [[PHLightState alloc] init];
+        lightState.on = @YES;
+        [lightState setBrightness:[NSNumber numberWithInt:254]];
+        [lightState setSaturation:[NSNumber numberWithInt:254]];
+        
+        // Send lightstate to light
+        [bridgeSendAPI updateLightStateForId:light.identifier withLightState:lightState completionHandler:^(NSArray *errors) {
+            if (errors != nil) {
+                NSString *message = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Errors", @""), errors != nil ? errors : NSLocalizedString(@"none", @"")];
+                
+                NSLog(@"Response: %@",message);
+            }
+            
+            [self.randomLightsButton setEnabled:YES];
+        }];
+    }
+
+}
+
+- (void) receiveSleepNote: (NSNotification*) note
+{
+    NSLog(@"Sleep");
+    
+    PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
+    PHBridgeSendAPI *bridgeSendAPI = [[PHBridgeSendAPI alloc] init];
+    
+    for (PHLight *light in cache.lights.allValues) {
+        
+        PHLightState *lightState = [[PHLightState alloc] init];
+        lightState.on = @NO;
+        [lightState setBrightness:[NSNumber numberWithInt:254]];
+        [lightState setSaturation:[NSNumber numberWithInt:254]];
+        
+        // Send lightstate to light
+        [bridgeSendAPI updateLightStateForId:light.identifier withLightState:lightState completionHandler:^(NSArray *errors) {
+            if (errors != nil) {
+                NSString *message = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Errors", @""), errors != nil ? errors : NSLocalizedString(@"none", @"")];
+                
+                NSLog(@"Response: %@",message);
+            }
+            
+            [self.randomLightsButton setEnabled:YES];
+        }];
+    }
+
+}
+
+- (void) fileNotifications
+{
+    //These notifications are filed on NSWorkspace's notification center, not the default
+    // notification center. You will not receive sleep/wake notifications if you file
+    //with the default notification center.
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver: self
+                                                           selector: @selector(receiveWakeNote:)
+                                                               name: NSWorkspaceDidWakeNotification object: NULL];
+    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver: self
+                                                           selector: @selector(receiveSleepNote:)
+                                                               name: NSWorkspaceWillSleepNotification object: NULL];
+
+}
+
+@end
